84d92c76df1ffb7b2483cea009f293c88a6cb753
Tak Hogan <takhogan@gmail.com>
2022-09-09 12:03:38 -0400

    daily commit

--- LogsViewer/src/log-loader.service.ts
+++ LogsViewer/src/log-loader.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject, Subject, Subscription } from 'rxjs';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams, HttpRequest, HttpResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LogLoaderService {
+export class LogLoaderService implements OnInit {
 
   private logList : Array<LogObject>
   private selectedLogIndex : number;
@@ -15,9 +16,37 @@ export class LogLoaderService {
   selectedLogIndex$ = selectedLogIndexSource.asObservable();
   logList$ = logListSource.asObservable();
 
-  constructor() {
+  constructor(private http : HttpClient) {
     this.logList = [];
     this.selectedLogIndex = null;
+  }
+
+  ngOnInit() {
+    let params = new HttpParams();
+      const req = new HttpRequest('POST', this.currentScript.props.deploymentTargetURL, formData);
+      // let videoData : VideoData;
+      this.subs.push(this.http.request(req).subscribe(event => {
+        // this.videoDataLoadStatusSource.next('started');
+        console.log('event body: ', event["body"]);
+      },
+      (err) => {
+        if ((err !== undefined) && (err !== null)) {
+          console.log('Upload Error! ', err);
+        } else {
+          console.log('Upload Error! undefined');
+        }
+        this.deployingScriptSource.next(false);
+        // this.videoDataLoadStatusSource.next('failed');
+      },
+      () => {
+        this.deployingScriptSource.next(false);
+        // this.updateVideoSrc(videoSrc);
+        // this.videoDataInit(videoData);
+        // this.videoDataLoadStatusSource.next('finished');
+        // window.setTimeout(() => {
+        //   this.videoDataLoadStatusSource.next('none');
+        // }, 4000);
+      }));
   }
 
 
