4f5a7d7740daf336e3745ce01c70aa584bd1076c
takhogan <takhogan@gmail.com>
2025-11-15 21:20:10 -0500

    escape args for windows cmd

--- runStart.bat
+++ runStart.bat
@@ -1,3 +1,8 @@
 @echo off
 set "PYTHONPATH=..\ScriptEngine"
-..\ScriptEngine\venv\Scripts\python.exe -m ScriptEngine.script_manager %*
+setlocal enabledelayedexpansion
+set "args="
+for %%a in (%*) do (
+    set "args=!args! "%%a""
+)
+..\ScriptEngine\venv\Scripts\python.exe -m ScriptEngine.script_manager!args!
